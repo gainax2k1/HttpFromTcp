b09db5001d7e67d95c3853db31ed5fd314bb9b80
gainax2k1 <gainax2k1@pm.me>
2026-02-18 15:25:47 -0800

    lesson 3

--- main.go
+++ main.go
@@ -2,8 +2,20 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"os"
 )
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	msgs, err := os.Open("messages.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer msgs.Close()
+
+	msgBuffer := make([]byte, 8)
+
+	for bytesRead, err := msgs.Read(msgBuffer); err != io.EOF; bytesRead, err = msgs.Read(msgBuffer) {
+		fmt.Printf("read: %s\n", string(msgBuffer[:bytesRead]))
+	}
 }
