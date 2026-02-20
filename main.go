17a3a375f0ca9488c2e3d835084afa3c9c2ae580
gainax2k1 <gainax2k1@pm.me>
2026-02-19 18:25:58 -0800

    Les 4 complete

--- main.go
+++ main.go
@@ -17,30 +17,40 @@ func main() {
 	msgBuffer := make([]byte, 8)
 	var currentLine string
 
-	for bytesRead, err := msgs.Read(msgBuffer); err != io.EOF; bytesRead, err = msgs.Read(msgBuffer) {
-		if err != nil {
-			panic(err)
-		}
+	for {
+		bytesRead, err := msgs.Read(msgBuffer)
 
-		currentLine += string(msgBuffer[:bytesRead])
+		if bytesRead > 0 {
+			currentLine += string(msgBuffer[:bytesRead])
+			//check for newlines in currentLine
+			if strings.Contains(currentLine, "\n") {
+				//split currentLine by newlines
+				splitStrings := strings.Split(currentLine, "\n")
 
-		//check for newlines in currentLine
-		if strings.Contains(currentLine, "\n") {
-			//split currentLine by newlines
-			splitStrings := strings.Split(currentLine, "\n")
-
-			for i, line := range splitStrings {
-				//  print each line except the last one, which may be incomplete
-				if i == len(splitStrings)-1 {
-					// last line, may be incomplete, save it for the next read
-					currentLine = line
-				} else {
-					fmt.Printf("read: %s\n", line)
+				for i, line := range splitStrings {
+					//  print each line except the last one, which may be incomplete
+					if i == len(splitStrings)-1 {
+						// last line, may be incomplete, save it for the next read
+						currentLine = line
+					} else {
+						fmt.Printf("read: %s\n", line)
+					}
 				}
+
 			}
 
 		}
-		// old output
-		//fmt.Printf("read: %s\n", string(msgBuffer[:bytesRead]))
+
+		if err != nil {
+			if err == io.EOF {
+				break // end of file reached, exit loop.
+			}
+			panic(err)
+		}
+
+	}
+	// print any remaining text in currentLine after the loop (in case last line does not end with a newline)
+	if len(currentLine) > 0 {
+		fmt.Printf("read: %s\n", currentLine)
 	}
 }
