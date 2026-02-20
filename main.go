c2818a27897b423e7810e73c8f96902d3c387e13
gainax2k1 <gainax2k1@pm.me>
2026-02-19 18:05:31 -0800

    les 4

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -14,8 +15,32 @@ func main() {
 	defer msgs.Close()
 
 	msgBuffer := make([]byte, 8)
+	var currentLine string
 
 	for bytesRead, err := msgs.Read(msgBuffer); err != io.EOF; bytesRead, err = msgs.Read(msgBuffer) {
-		fmt.Printf("read: %s\n", string(msgBuffer[:bytesRead]))
+		if err != nil {
+			panic(err)
+		}
+
+		currentLine += string(msgBuffer[:bytesRead])
+
+		//check for newlines in currentLine
+		if strings.Contains(currentLine, "\n") {
+			//split currentLine by newlines
+			splitStrings := strings.Split(currentLine, "\n")
+
+			for i, line := range splitStrings {
+				//  print each line except the last one, which may be incomplete
+				if i == len(splitStrings)-1 {
+					// last line, may be incomplete, save it for the next read
+					currentLine = line
+				} else {
+					fmt.Printf("read: %s\n", line)
+				}
+			}
+
+		}
+		// old output
+		//fmt.Printf("read: %s\n", string(msgBuffer[:bytesRead]))
 	}
 }
